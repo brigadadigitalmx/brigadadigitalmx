ec0e14f0aa425b1404475608846701b56c6c5821
Emir Herrera <emir.herrera@gmail.com>
2020-04-20 12:36:12 -0500

    add: dev: Beta release React WP Theme
    Demo of a WordPress Theme working with React

---
+++ precache-manifest.f53f29450b28559c00583d2dd33a3518.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7867f7f478de00e1dd0831e3085fce",
+    "url": "/wp-content/themes/brigadadigitalmx/index.php"
+  },
+  {
+    "revision": "1f1f2140707a8c77f2f2",
+    "url": "/wp-content/themes/brigadadigitalmx/static/js/2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wp-content/themes/brigadadigitalmx/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5a9011d75e79603e81",
+    "url": "/wp-content/themes/brigadadigitalmx/static/js/bundle.js"
+  },
+  {
+    "revision": "4486fee75ba19031f888",
+    "url": "/wp-content/themes/brigadadigitalmx/static/js/main.chunk.js"
+  },
+  {
+    "revision": "9893d967220bc41e2d091e305f8fc348",
+    "url": "/wp-content/themes/brigadadigitalmx/static/media/logo.9893d967.png"
+  },
+  {
+    "revision": "6004b7a7e70aba48806566b6e8d9decf",
+    "url": "/wp-content/themes/brigadadigitalmx/static/media/sociedat.6004b7a7.jpg"
+  }
+]);
