f77b49e7c7fc5eba7eb24a9ecc371e6c19f2b2a1
ArodalvGU <55214623+ArodalvGU@users.noreply.github.com>
2020-04-20 12:05:14 -0400

    Update Faqs.tsx

--- src/components/Faqs.tsx
+++ src/components/Faqs.tsx
@@ -13,11 +13,11 @@ export const Faqs = () => {
         <h1 className="cormorant big">Gobiernos</h1>
         <div className="margin-top-one padding-left-one">
           <ListedFaq
-            question="¿Cómo solicito un voluntario?"
-            answer="Complete este formulario y comparta la mayor cantidad de información posible sobre el problema que está tratando de resolver y el apoyo que cree que necesita."
+            question="¿Cómo solicitar ayuda?"
+            answer="Completa este formulario y comparte la mayor cantidad de información posible sobre el problema que estás tratando de resolver y el apoyo que se requiera."
           />
           <ListedFaq
-            question="¿Los voluntarios deberán convertirse en empleados del gobierno?"
+            question="¿Los voluntarios deberán convertirse en empleados de gobierno?"
             answer="No. Los equipos de gobierno son los encargados de definir las actividades y el acceso que los voluntarios tienen."
           />
           <ListedFaq
@@ -34,7 +34,7 @@ export const Faqs = () => {
           />
           <ListedFaq
             question="¿Quién gestiona el proyecto?"
-            answer="Usted y el voluntario son responsables de administrar el proyecto. Lo conectaremos con voluntarios calificados, pero el éxito del proyecto depende de su colaboración directa."
+            answer="La gestión del proyecto será bilateral, tanto la dependencia como los voluntarios son responsables de administrar el proyecto. El equipo asignado estará calificado, pero el éxito del proyecto depende de la involucración activa y directa."
           />
           <div className="buttons-list margin-top-three">
             <a
@@ -81,19 +81,19 @@ export const Faqs = () => {
           />
           <ListedFaq
             question="¿Cómo examina la Brigada Digital Mx a los voluntarios?"
-            answer="Los voluntarios envían una solicitud que es revisada por el equipo de Brigada Digital Mx. Si creemos que el solicitante podría ser una buena opción para un proyecto, se le asigna un equipo y un líder de proyecto. La asignación del proyecto depende de la urgencia e importancia de nuestros proyectos y de la experiencia y preparación necesaria de cada proyecto. "
+            answer="Los voluntarios envían una solicitud que es revisada por el equipo de Brigada Digital Mx. Si creemos que el solicitante podría ser una buena opción para un proyecto, se le asigna un equipo y un líder de proyecto. La asignación del proyecto depende de la urgencia e importancia de nuestros proyectos y de la experiencia y preparación necesaria de cada proyecto."
           />
           <ListedFaq
             question="¿Quién gestiona el proyecto?"
-            answer="El voluntario y el enlace de gobierno son los  responsables de administrar el proyecto. Conectamos voluntarios con gobiernos que necesitan ayuda, pero el éxito del proyecto depende de su colaboración directa. Brigada Digital MX hará lo posible para apoyar los proyectos establecidos."
+            answer="El voluntario o en su caso el líder de proyecto en conjunto con el enlace de gobierno son los  responsables de administrar el proyecto. Conectamos voluntarios con gobiernos que necesitan ayuda, pero el éxito del proyecto depende de su colaboración directa. Brigada Digital MX hará lo posible para apoyar los proyectos establecidos."
           />
           <ListedFaq
-            question="¿Donde trabajare?"
-            answer="Si encontramos a un proyecto que necesite tus características, serás asignado a un proyecto y  trabajarás con una agencia gubernamental estatal o local de manera digital y remota."
+            question="¿Donde trabajaré?"
+            answer="Si encontramos a un proyecto que necesite tus características, serás asignado a él  y trabajarás con una agencia gubernamental, estatal o local de manera digital y remota."
           />
           <ListedFaq
             question="¿Cuánto durarán los proyectos?"
-            answer="Nuestros proyectos tienen diferente naturaleza y variarán en sus tiempos."
+            answer="Nuestros proyectos tienen diferentes enfoques y alcances, cada uno variará en el tiempo requerido."
           />
           <div className="buttons-list margin-top-three">
             <a
